3efd29167ba76425d8dbe24cef5ecacde39dcccd
Ilya <i@makarskij.com>
2024-03-09 19:29:17 +0100

    #1. Populate users with random data

--- queries/1.populate_users.sql
+++ queries/1.populate_users.sql
@@ -9,48 +9,34 @@ DELIMITER $$
 CREATE PROCEDURE generate_data()
 BEGIN
     DECLARE i INT DEFAULT 0;
-    DECLARE j INT DEFAULT 0;
-    DECLARE createdAt DATETIME;
-    DECLARE updatedAt DATETIME;
+    DECLARE createdAt1 DATETIME;
+    DECLARE updatedAt1 DATETIME;
+    DECLARE createdAt2 DATETIME;
+    DECLARE updatedAt2 DATETIME;
 
-    WHILE i < 100 DO
-        SET createdAt = TIMESTAMPADD(SECOND, FLOOR(RAND() * TIMESTAMPDIFF(SECOND, '2022-01-01 00:00:00', '2023-12-31 23:59:59')), '2022-01-01 00:00:00');
-        SET updatedAt = TIMESTAMPADD(SECOND, FLOOR(RAND() * TIMESTAMPDIFF(SECOND, createdAt, '2023-12-31 23:59:59')), createdAt);
-        SET @query =
-            CONCAT(
-                'INSERT INTO `users` (`name`,`email`,`password`,`language`,`created_at`,`updated_at`)
-                VALUES (
+    WHILE i < 500000 DO
+        SET createdAt1 = TIMESTAMPADD(SECOND, FLOOR(RAND() * TIMESTAMPDIFF(SECOND, '2022-01-01 00:00:00', '2023-12-31 23:59:59')), '2022-01-01 00:00:00');
+        SET updatedAt1 = TIMESTAMPADD(SECOND, FLOOR(RAND() * TIMESTAMPDIFF(SECOND, createdAt1, '2023-12-31 23:59:59')), createdAt1);
+        SET createdAt2 = TIMESTAMPADD(SECOND, FLOOR(RAND() * TIMESTAMPDIFF(SECOND, '2022-01-01 00:00:00', '2023-12-31 23:59:59')), '2022-01-01 00:00:00');
+        SET updatedAt2 = TIMESTAMPADD(SECOND, FLOOR(RAND() * TIMESTAMPDIFF(SECOND, createdAt2, '2023-12-31 23:59:59')), createdAt2);
+
+            INSERT INTO `users` (`name`,`email`,`password`,`language`,`created_at`,`updated_at`)
+            VALUES (
                 LEFT(UUID(), 10 + RAND() * 20),
-                CONCAT(UUID(), "@example.com"),
+                CONCAT(UUID(), '@example.com'),
                 LEFT(UUID(), 8 + RAND() * 16),
-                ELT(FLOOR(RAND() * 5) + 1, "be", "cz", "en", "lv", "pl"),"',
-                createdAt,
-                '","',
-                updatedAt,
-                '")'
+                ELT(FLOOR(RAND() * 5) + 1, 'be', 'cz', 'en', 'lv', 'pl'),
+                createdAt1,
+                updatedAt1
+            ),
+            (
+               LEFT(UUID(), 10 + RAND() * 20),
+               CONCAT(UUID(), '@example.com'),
+               LEFT(UUID(), 8 + RAND() * 16),
+               ELT(FLOOR(RAND() * 5) + 1, 'be', 'cz', 'en', 'lv', 'pl'),
+               createdAt2,
+               updatedAt2
             );
-
-        WHILE j < 99 DO
-            SET createdAt = TIMESTAMPADD(SECOND, FLOOR(RAND() * TIMESTAMPDIFF(SECOND, '2022-01-01 00:00:00', '2023-12-31 23:59:59')), '2022-01-01 00:00:00');
-            SET updatedAt = TIMESTAMPADD(SECOND, FLOOR(RAND() * TIMESTAMPDIFF(SECOND, createdAt, '2023-12-31 23:59:59')), createdAt);
-            SET @query = CONCAT(
-                @query,
-                ',(
-                     LEFT(UUID(), 10 + RAND() * 20),
-                     CONCAT(UUID(), "@example.com"),
-                     LEFT(UUID(), 8 + RAND() * 16),
-                     ELT(FLOOR(RAND() * 5) + 1, "be", "cz", "en", "lv", "pl"),"',
-                     createdAt,
-                    '","',
-                     updatedAt,
-                '")'
-            );
-            SET j = j + 1;
-        END WHILE;
-
-        PREPARE myquery FROM @query;
-        EXECUTE myquery;
-        SET j = 0;
         SET i = i + 1;
     END WHILE;
 END$$
