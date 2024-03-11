39ef42d8479034f5037e34e80a6578a1d1952f1b
Ilya <i@makarskij.com>
2024-03-11 14:29:49 +0100

    #11. Fix transactions table inserion

--- problems/5/db-structure-and-data.sql
+++ problems/5/db-structure-and-data.sql
@@ -2,20 +2,7 @@
 # Need to generate millions of transactions
 
 # Solution:
-
-CREATE TABLE transactions (
-    id INT NOT NULL AUTO_INCREMENT,
-    order_id VARCHAR(255) NOT NULL,
-    transaction_id VARCHAR(255) NOT NULL,
-    user_id INT NOT NULL,
-    store ENUM('appstore', 'googleplay', 'aptoide', 'f-droid') NOT NULL,
-    price INT NOT NULL,
-    product_id VARCHAR(255) NOT NULL,
-    cancelled_at DATETIME,
-    created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    PRIMARY KEY (id),
-    FOREIGN KEY (user_id) REFERENCES users(id)
-);
+# Runtime ~240 seconds
 
 DROP PROCEDURE IF EXISTS generate_data;
 
@@ -28,7 +15,7 @@ BEGIN
     DECLARE createdAt DATETIME;
     DECLARE productId VARCHAR(18);
 
-    WHILE i < 1000 DO
+    WHILE i < 1415 DO
         SET createdAt = TIMESTAMPADD(SECOND, FLOOR(RAND() * TIMESTAMPDIFF(SECOND, '2022-01-01 00:00:00', '2023-12-31 23:59:59')), '2022-01-01 00:00:00');
         SET cancelledAt =
             CONCAT(
@@ -37,24 +24,12 @@ BEGIN
                 '"'
             );
         SET productId = ELT(FLOOR(RAND() * 4) + 1, 'subscription.week', 'subscription.month', 'product1', 'product2');
-        SET cancelledAt = IF(productId IN('product1', 'product2'), 'NULL', cancelledAt);        SET @query =
-            CONCAT(
+        SET cancelledAt = IF(productId IN('product1', 'product2'), 'NULL', cancelledAt);
+        SET @query =
                 'INSERT INTO `transactions` (`order_id`,`transaction_id`,`user_id`,`store`,`price`,`product_id`,`cancelled_at`,`created_at`)
-                VALUES (
-                    UUID(),
-                    UUID(),
-                    FLOOR(RAND() * 2000000) + 1,
-                    ELT(FLOOR(RAND() * 4) + 1, "appstore", "googleplay", "aptoide", "f-droid"),
-                    FLOOR(RAND() * 30) + 1,"',
-                    productId,
-                    '",',
-                    cancelledAt,
-                    ',"',
-                    createdAt,
-                '")'
-            );
+                VALUES ';
 
-        WHILE j < 999 DO
+        WHILE j < 1415 DO
             SET createdAt = TIMESTAMPADD(SECOND, FLOOR(RAND() * TIMESTAMPDIFF(SECOND, '2022-01-01 00:00:00', '2023-12-31 23:59:59')), '2022-01-01 00:00:00');
             SET cancelledAt =
                 CONCAT(
@@ -62,11 +37,11 @@ BEGIN
                     TIMESTAMPADD(SECOND, FLOOR(RAND() * TIMESTAMPDIFF(SECOND, createdAt, '2023-12-31 23:59:59')), createdAt),
                     '"'
                 );
-            SET productId = ELT(FLOOR(RAND() * 4) + 1, 'subscription.week', 'subscription.month', 'product1', 'product2');
             SET cancelledAt = IF(productId IN('product1', 'product2'), 'NULL', cancelledAt);
             SET @query = CONCAT(
                 @query,
-                ',(
+                IF(j = 0, '', ','),
+                '(
                     UUID(),
                     UUID(),
                     FLOOR(RAND() * 2000000) + 1,
