f9fe313d063edae7bc677fc2aa78d364861a7bd2
Ilya <i@makarskij.com>
2024-03-12 13:11:46 +0100

    #13. Get transactions on a specific day

---
+++ problems/6/problem.sql
@@ -0,0 +1,1 @@
+# Get transactions on a specific day
---
+++ problems/6/solution.sql
@@ -0,0 +1,6 @@
+# Runtime ~0.01 seconds
+SELECT transactions.id, transactions.order_id, transactions.transaction_id, transactions.user_id, transactions.store, transactions.price, transactions.product_id, transactions.cancelled_at, transactions.created_at, users.email, users.name
+FROM transactions
+JOIN users
+ON transactions.user_id = users.id
+WHERE DATE(transactions.created_at) = '2023-01-01'
