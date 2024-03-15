332fe358fe350b4b163b1b806174569eaab0a531
Ilya <i@makarskij.com>
2024-03-15 23:51:07 +0100

    #21. Fix query

--- problems/10/solution.sql
+++ problems/10/solution.sql
@@ -1,10 +1,8 @@
-# Runtime ~22 seconds
-SELECT COUNT(user_id)
-FROM `transactions`
-WHERE product_id IN ('subscription.week', 'subscription.month') AND user_id IN (
+# Runtime ~20 seconds
+SELECT COUNT(t.user_id) from (
     SELECT user_id
-    FROM transactions
-    WHERE cancelled_at IS NULL
-    GROUP BY user_id
-    HAVING COUNT(product_id IN ('subscription.week', 'subscription.month')) > 1
-)
+        FROM transactions
+        WHERE cancelled_at IS NULL and product_id  IN ('subscription.week', 'subscription.month')
+        GROUP BY user_id
+        HAVING count(user_id) > 1
+) as t
