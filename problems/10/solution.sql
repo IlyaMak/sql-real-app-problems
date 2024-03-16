bbfbdb642e599c8c701a1d0df8a0ae62fbd0edef
Ilya <i@makarskij.com>
2024-03-16 15:22:50 +0100

    #21. Refactoring

--- problems/10/solution.sql
+++ problems/10/solution.sql
@@ -2,7 +2,7 @@
 SELECT COUNT(t.user_id) from (
     SELECT user_id
         FROM transactions
-        WHERE cancelled_at IS NULL and product_id  IN ('subscription.week', 'subscription.month')
+        WHERE cancelled_at IS NULL and product_id IN ('subscription.week', 'subscription.month')
         GROUP BY user_id
-        HAVING count(user_id) > 1
+        HAVING COUNT(user_id) > 1
 ) as t
