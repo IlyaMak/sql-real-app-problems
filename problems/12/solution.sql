46b0356d6c452bf24c064171a9e75e9bd5aa3265
Ilya <i@makarskij.com>
2024-03-17 16:57:36 +0100

    #25. Replace join with inner join

--- problems/12/solution.sql
+++ problems/12/solution.sql
@@ -2,7 +2,7 @@
 SELECT ROUND(COUNT(t.user_id) * 100 / (SELECT COUNT(*) FROM `users`), 2) AS percentage FROM (
     SELECT DISTINCT users.id AS user_id
     FROM `users`
-    JOIN `transactions`
+    INNER JOIN `transactions`
     ON users.id = transactions.user_id
     WHERE users.created_at > transactions.created_at
 ) AS t
